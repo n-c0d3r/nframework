10d589c48696b05202575606c8023c65ae2c5bcc
dnblue-code <68686710+dnblue-code@users.noreply.github.com>
2021-09-01 16:30:13 +0700

    add uninstall.bat and install.bat

--- nframework/cli/install.bat
+++ nframework/cli/install.bat
@@ -1,2 +1,1 @@
-cd cli
 npm install -g .
--- nlc/pages/home/home.client.base.nlc.server.js
+++ nlc/pages/home/home.client.base.nlc.server.js
@@ -1,5 +1,5 @@
 var JSCLPath = "D:\\nframework_gr/nlc/pages/home/home.client.base.nlc.client.js";
-                            var a6f4568ba_add2_49e6_a5a4_42ccfa2bc142_module;
+                            var a5648f433_6a0f_48a6_a06e_ba11e0bc53e6_module;
                         module.exports=(manager)=>{
                 var exports=new Object();
                     var nmodules=[];
@@ -43,14 +43,14 @@ var JSCLPath = "D:\\nframework_gr/nlc/pages/home/home.client.base.nlc.client.js"
 
             function(){
                 this.GetThisWithCallback((module)=>{
-                            a6f4568ba_add2_49e6_a5a4_42ccfa2bc142_module=module;
+                            a5648f433_6a0f_48a6_a06e_ba11e0bc53e6_module=module;
                         })
-                        var getterObj6f4568ba_add2_49e6_a5a4_42ccfa2bc142={
+                        var getterObj5648f433_6a0f_48a6_a06e_ba11e0bc53e6={
                             set stter(value) {
-                                a6f4568ba_add2_49e6_a5a4_42ccfa2bc142_module.Set('syncProp',value);
+                                a5648f433_6a0f_48a6_a06e_ba11e0bc53e6_module.Set('syncProp',value);
                             }
                         }
-                        getterObj6f4568ba_add2_49e6_a5a4_42ccfa2bc142.stter=0; 
+                        getterObj5648f433_6a0f_48a6_a06e_ba11e0bc53e6.stter=0; 
             }   
 
         
