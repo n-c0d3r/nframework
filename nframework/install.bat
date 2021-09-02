ef34438e5f8783b5f404d3c006cc935fad390b4e
dnblue-code <68686710+dnblue-code@users.noreply.github.com>
2021-09-02 15:46:28 +0700

    Revert "add vscode setting for nlc init."
    This reverts commit 42b22257744ea417e1441b50272d3a14d1c2c405.

--- nframework/install.bat
+++ nframework/install.bat
@@ -1,1 +1,2 @@
+cd cli
 npm install -g .
--- nlc/pages/home/home.client.base.nlc.server.js
+++ nlc/pages/home/home.client.base.nlc.server.js
@@ -1,5 +1,9 @@
 var JSCLPath = "D:\\nframework_gr/nlc/pages/home/home.client.base.nlc.client.js";
+<<<<<<< HEAD
                             var ad0a15c74_e5f3_4332_b824_57b153ea3911_module;
+=======
+                            var a6f4568ba_add2_49e6_a5a4_42ccfa2bc142_module;
+>>>>>>> parent of 10d589c (add uninstall.bat and install.bat)
                         module.exports=(manager)=>{
                 var exports=new Object();
                     var nmodules=[];
@@ -43,6 +47,7 @@ var JSCLPath = "D:\\nframework_gr/nlc/pages/home/home.client.base.nlc.client.js"
 
             function(){
                 this.GetThisWithCallback((module)=>{
+<<<<<<< HEAD
                             ad0a15c74_e5f3_4332_b824_57b153ea3911_module=module;
                         })
                         var getterObjd0a15c74_e5f3_4332_b824_57b153ea3911={
@@ -51,6 +56,16 @@ var JSCLPath = "D:\\nframework_gr/nlc/pages/home/home.client.base.nlc.client.js"
                             }
                         }
                         getterObjd0a15c74_e5f3_4332_b824_57b153ea3911.stter=0; 
+=======
+                            a6f4568ba_add2_49e6_a5a4_42ccfa2bc142_module=module;
+                        })
+                        var getterObj6f4568ba_add2_49e6_a5a4_42ccfa2bc142={
+                            set stter(value) {
+                                a6f4568ba_add2_49e6_a5a4_42ccfa2bc142_module.Set('syncProp',value);
+                            }
+                        }
+                        getterObj6f4568ba_add2_49e6_a5a4_42ccfa2bc142.stter=0; 
+>>>>>>> parent of 10d589c (add uninstall.bat and install.bat)
             }   
 
         
