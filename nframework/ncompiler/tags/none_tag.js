8baba71e8ffafd2e64052f8c42c878ce227cbff4
silverbosstd <sologame37@gmail.com>
2021-10-31 10:21:06 +0700

    Fix none-tag.js

--- nframework/ncompiler/tags/none_tag.js
+++ nframework/ncompiler/tags/none_tag.js
@@ -4,38 +4,10 @@ let tag = new Tag();
 
 tag.isAutoClose = false;
 
-
 tag.Compile = function(element, childsCode, code, manager, nlcPath) {
-
     let line = 0;
 
-    for (let i = 0; i <
-        var Tag = require('../tag/tag');
-
-        var tag = new Tag();
-
-        tag.isAutoClose = false;
-
-
-        tag.Compile = function(element, childsCode, code, manager, nlcPath) {
-
-            var line = 0;
-
-            for (var i = 0; i < element.startContentIndex; i++) {
-                if (code.data[i] == '\n') {
-                    line++;
-                }
-            }
-
-            console.log(`${nlcPath}:${line}`);
-            console.log(`   '${tag.notFoundName}' tag not found.`);
-            process.exit();
-            return `
-    `;
-        }
-
-
-        module.exports = tag; element.startContentIndex; i++) {
+    for (let i = 0; i < element.startContentIndex; i++) {
         if (code.data[i] == '\n') {
             line++;
         }
